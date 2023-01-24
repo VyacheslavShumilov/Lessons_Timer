6b516ddc7fc42c48e7c89a26e6c4ba386fb3b3f1
user.email <vvshumilov@mail.ru>
2023-01-24 18:18:38 +0300

    App & Constants

---
+++ app/src/main/java/com/vshum/lessonstimer/App.kt
@@ -0,0 +1,20 @@
+package com.vshum.lessonstimer
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(
+                Modules.application,
+                Modules.bottomNavigationWindow,
+                Modules.homeWindow,
+                Modules.classesWindow,
+            )
+        }
+    }
+}
