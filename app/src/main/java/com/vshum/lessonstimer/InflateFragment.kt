aff037f53dcf7f1253d572df6933c7a4a0c63de9
user.email <vvshumilov@mail.ru>
2023-01-24 18:22:52 +0300

    BaseFragment

---
+++ app/src/main/java/com/vshum/lessonstimer/InflateFragment.kt
@@ -0,0 +1,6 @@
+package com.vshum.lessonstimer
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+typealias InflateFragment<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
--- app/src/main/java/com/vshum/lessonstimer/MainActivity.kt
+++ app/src/main/java/com/vshum/lessonstimer/MainActivity.kt
@@ -2,10 +2,67 @@ package com.vshum.lessonstimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import org.koin.core.qualifier.named
+import org.koin.java.KoinJavaComponent
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = KoinJavaComponent.getKoin().createScope<MainActivity>()
+    private var navigatorHolder: NavigatorHolder = scope.get(qualifier = named(Scopes.NAVIGATOR_TAB))
+    private val router: Router = scope.get(qualifier = named(Scopes.ROUTER))
+    private val navigator = AppNavigator(this, R.id.bottom_container)
+
+    private val binding: MainActivityBinding by lazy { MainActivityBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        router.newRootScreen(HomeScreen())
+        setBottomClickListener()
     }
-}
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
+    }
+
+    /** Задать обработчик перехода по табам */
+    private fun setBottomClickListener() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                //основной экран
+                R.id.menu_bottom_home -> {
+                    router.newRootScreen(HomeScreen())
+                    true
+                }
+
+                //уроки
+                R.id.menu_bottom_classes -> {
+                    router.newRootScreen(ClassesScreen())
+                    true
+                }
+
+                //Список
+                R.id.menu_bottom_list -> {
+                    router.newRootScreen(ListScreen())
+                    true
+                }
+
+                //теги
+                R.id.menu_bottom_tag -> {
+                    router.newRootScreen(TagScreen())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
