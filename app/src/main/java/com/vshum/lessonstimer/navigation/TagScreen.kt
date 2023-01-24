c1a04f0311d1c52bf7a5f6f6d9d10f19f6b7a429
user.email <vvshumilov@mail.ru>
2023-01-24 18:33:34 +0300

    screens

---
+++ app/src/main/java/com/vshum/lessonstimer/navigation/TagScreen.kt
@@ -0,0 +1,9 @@
+package com.vshum.lessonstimer.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+class TagScreen(): FragmentScreen {
+    override fun createFragment(factory: FragmentFactory): Fragment = TagFragment()
+}
